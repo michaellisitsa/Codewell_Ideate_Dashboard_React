02c4b1867d51e3fba2c2c6430aee0db6a9c81d31
Michael Lisitsa <michaellisitsa@Michaels-MacBook-Pro-2.local>
2021-10-20 22:01:43 +1100

    Copy React Table starter code

---
+++ ideate-dashboard/src/components/Table.js
@@ -0,0 +1,37 @@
+import { useTable } from "react-table";
+import makeData from "./makeData";
+
+function Table({ columns, data }) {
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({
+      columns,
+      data,
+    });
+
+  //Render the Table UI
+  return (
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map((headerGroup) => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row, i) => {
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map((cell) => {
+                return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
